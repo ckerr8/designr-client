5788f243691ce30364f4f43ea37dd0d707ab946a
Chelsea Kerr <54897046+ckerr8@users.noreply.github.com>
2024-09-18 10:27:49 -0400

    add projects view

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import { Container, Card } from 'semantic-ui-react'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Projects from "./pages/Projects/Projects";
 import "./App.scss";
 
 function App() {
   return (
-    <>
-    <Dashboard />
-      <Card></Card>
-    </>
+    <BrowserRouter>
+    <Routes>
+    <Route 
+    path='/' 
+    element={<Dashboard />} />
+    <Route 
+    path='/projects' 
+    element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,6 +1,8 @@
 import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
 import '../../styles/global.scss'
+import { Card, Image, Header } from 'semantic-ui-react';
+import './Projects.scss'
 
 export default function Dashboard() {
         return (
@@ -8,6 +10,26 @@ export default function Dashboard() {
           <div className="screen">
           <Navigation />
           <div className="screen__content">
+          <Card className="wrap">
+            <Image className="wrap__img" src="../../src/assets/images/bold.png"></Image>
+            <Header>
+            Card</Header>
+            </Card>
+            <Card className="wrap">
+            <Image className="wrap__img" src="../../src/assets/images/bold.png"></Image>
+            <Header>
+            Card</Header>
+            </Card>
+            <Card className="wrap">
+            <Image className="wrap__img" src="../../src/assets/images/bold.png"></Image>
+            <Header>
+            Card</Header>
+            </Card>
+            <Card className="wrap">
+            <Image className="wrap__img" src="../../src/assets/images/bold.png"></Image>
+            <Header>
+            Card</Header>
+            </Card>
           </div>
           </div>
           <Footer />
